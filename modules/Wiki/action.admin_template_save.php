48e4a6a8426aa20e7ba2bf5ee67a74667cf2bde5
besstiolle <contact@furie.be>
2015-04-19 14:42:10 +0200

    fix saving the access template

--- modules/Wiki/action.admin_template_save.php
+++ modules/Wiki/action.admin_template_save.php
@@ -3,9 +3,8 @@
 if (!function_exists("cmsms")) exit;
 
 if(isset($params['template_access'])){
-	$this->SetTemplate('template_access',$params['template_access']);
+	$this->SetTemplate('access',$params['template_access']);
 }
-
 $this->RedirectToAdminTab('tab3');
 
 
