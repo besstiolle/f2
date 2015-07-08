54c099c917d1e7d8184ee434df9341a2ffcda9ff
besstiolle <contact@furie.be>
2015-07-08 20:26:18 +0200

    fix warn in navigation

--- modules/Forge2FrontOffice/action.navigation.php
+++ modules/Forge2FrontOffice/action.navigation.php
@@ -4,11 +4,17 @@ if (!function_exists("cmsms")) exit;
 
 $config = cmsms()->GetConfig();
 $smarty->addTemplateDir($config['root_path'].'/modules/Forge2FrontOffice/templates'); 
+$smarty->assign('root_url', $config['root_url']);
 
 include('action.wikigateway.php');
 
+if(isset($project)){
+	$smarty->assign('is_admin', forge_utils::is_project_admin($project, forge_utils::getConnectedUserId()));
+	$smarty->assign('is_member', forge_utils::is_project_member($project, forge_utils::getConnectedUserId()));	
+} else {
+	$smarty->assign('is_admin', false);
+	$smarty->assign('is_member', false);
+}
 
-$smarty->assign('is_admin', forge_utils::is_project_admin($project, forge_utils::getConnectedUserId()));
-$smarty->assign('is_member', forge_utils::is_project_member($project, forge_utils::getConnectedUserId()));
 
 echo $smarty->display('inc_navigation.tpl');
