64c5e4a59bd58dc8bd64f6088d679860bac7ab95
besstiolle <contact@furie.be>
2015-07-11 21:50:57 +0200

    add ServiceAssignment

--- modules/Forge2FrontOffice/action.my_modules.php
+++ modules/Forge2FrontOffice/action.my_modules.php
@@ -10,33 +10,10 @@ if(!forge_utils::getConnectedUserId()){
 	forge_utils::inner_redirect('/account');
 }
 
-$restParameters = array(
-			'user_id' => $params['user_id'],
-			'n' => 100);
+$serviceAssignment = new ServiceAssignment();
+$assignments = $serviceAssignment->getByUserId($params['user_id']);
+if(!$assignments) { return; }
 
-//Ask the 100 modules
-$request = RestAPI::GET('rest/v1/assignment', $restParameters);
-
-if($request->getStatus() === 404){
-	/*$smarty->assign('error', 'you don\'t have any project');
-	echo $smarty->display('msg_notFound.tpl');
-	return;*/
-} else if($request->getStatus() !== 200){
-	//Debug part
-	$smarty->assign('error', "Error processing the Rest request");
-	echo $smarty->display('msg_rest_error.tpl');
-	include('lib/inc.debug.php');	
-	return;
-}
-
-$response = json_decode($request->getResponse(), true);
-
-//Get the assignments in the response data
-$assignments = $response['data']['assignments'];
-
-$config = cmsms()->GetConfig();
-
-$smarty->assign('root_url', $config['root_url']);
 $smarty->assign('assignments', $assignments);
 $smarty->assign('link_create', $config['root_url'].'/project/new');
 $smarty->assign('enumProjectState', Enum::ConstToArray('EnumProjectState'));
